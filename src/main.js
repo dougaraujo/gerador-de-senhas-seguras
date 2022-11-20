a6fb92919d13e7c01791b7b16414b55092fe62c3
Douglas Araújo <goudxs@gmail.com>
2022-11-20 17:30:59 -0300

    Implementa a função de copiar a senha com a biblioteca clipboard-copy

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import { nanoid } from "nanoid";
-import "./style.css"
+import "./style.css";
+import copy from "clipboard-copy";
 
 const passwdBtnEl = document.querySelector('button');
 const displayPasswdEl = document.querySelector('h2');
+const displayCopiedEl = document.querySelector('p');
 
 passwdBtnEl.addEventListener('click', () => {
     const randomPasswd = nanoid();
     displayPasswdEl.innerHTML = randomPasswd;
+    displayPasswdEl.addEventListener('click', () => {
+        copy(randomPasswd);
+        displayCopiedEl.classList.remove('not-display');
+        setTimeout(() => {
+            displayCopiedEl.classList.add('not-display');
+        }, 2500)
+    });
 });
+
+
 
 
